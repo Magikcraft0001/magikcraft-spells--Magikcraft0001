d25099c3b87bd3514034f57e20244fc0014250d7
Magikcraft0001 <tracyawulf@gmail.com>
2017-11-12 08:25:54 +0000

    Update terepo-to.ts

---
+++ terepo-to.ts
@@ -0,0 +1,11 @@
+const magik = magikcraft.io;
+
+function terepo-to() {
+    const there = magik.memento.getItem();
+    if (there) {
+        magik.ianuae(there);
+    } else {
+        const here = magik.hic();
+        magik.memento(here);
+    }
+}
